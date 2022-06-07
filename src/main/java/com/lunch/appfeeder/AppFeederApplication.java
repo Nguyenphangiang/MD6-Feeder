f14375832efd7692ff7269d59b075f95fe6725fe
Nguyenphangiang <phancruel@gmail.com>
2022-06-07 16:52:14 +0700

    create branch master

---
+++ src/main/java/com/lunch/appfeeder/AppFeederApplication.java
@@ -0,0 +1,13 @@
+package com.lunch.appfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFeederApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunch/appfeeder/AppFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunch.appfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
